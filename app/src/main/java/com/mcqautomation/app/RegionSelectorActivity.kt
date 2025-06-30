1191ce3a60b0c26f5a0ee4e580b449fb9f7bf176
anandk369 <anandk765898@gmail.com>
2025-06-30 21:25:12 +0530

    Add stubs for RegionSelectorActivity and PositioningActivity

---
+++ app/src/main/java/com/mcqautomation/app/RegionSelectorActivity.kt
@@ -0,0 +1,11 @@
+package com.mcqautomation.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegionSelectorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO: setContentView(R.layout.activity_region_selector)
+    }
+}
